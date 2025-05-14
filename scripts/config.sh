887e992baae1d3106edb251fe20602dcbd23d525
esequieledl <esequieledl@gmail.com>
2025-05-14 02:44:40 -0300

    update dotfiles

--- scripts/config.sh
+++ scripts/config.sh
@@ -14,6 +14,7 @@ set_all_stow() {
   set_stow tmux
   set_stow waybar
   set_stow zshrc
+	set_show git
 	set_stow backgrounds
 }
 
@@ -30,48 +31,20 @@ set_stow() {
 
 # configurations
 set_configs(){
+	set_git_config
+	set_zsh_config
+
   echo -e "\n${BLUE}==> Setting some configurations${GRAY}"
 
   ## apps config
-  mkdir -p /home/$USER_INPUT/.local/share/applications/
-
-  ### hyprland
-  #cp -r ${SCRIPT_PATH}/dotfiles/hypr ~/.config/
-  #cp -r ${SCRIPT_PATH}/backgrounds ~/
-
-  ### kitty
-  #cp -r ${SCRIPT_PATH}/dotfiles/kitty ~/.config/
+  #mkdir -p /home/$USER_INPUT/.local/share/applications/
 
   ### spotify
-  cp ${SCRIPT_PATH}/configs/spotify.desktop ~/.local/share/applications/spotify.desktop
-  echo "$PASS_INPUT" | sudo -S xdg-mime default spotify.desktop x-scheme-handler/spotify
-
-  ### neovim
-  mkdir -p ~/.config/nvim
-  touch ~/.config/nvim/init.vm
+  # cp ${SCRIPT_PATH}/configs/spotify.desktop ~/.local/share/applications/spotify.desktop
+  # echo "$PASS_INPUT" | sudo -S xdg-mime default spotify.desktop x-scheme-handler/spotify
 
   ### brave
-  cp ${SCRIPT_PATH}/configs/Preferences ~/.config/BraveSoftware/Brave-Browser/Default/
-
-  ### git
-  cp ${SCRIPT_PATH}/dotfiles/.gitignore_global ~/.gitignore_global
-  git config --global user.name "$USER_INPUT"
-  git config --global user.email "$EMAIL_INPUT"
-  git config --global color.ui auto
-  git config --global core.editor "nvim"
-  git config --global alias.st status
-  git config --global alias.co checkout
-  git config --global alias.br branch
-  git config --global alias.cm commit
-  git config --global alias.last 'log -1 HEAD'
-  git config --global alias.rv 'reset --soft HEAD~1'
-  git config --global alias.lg 'log --oneline --graph --decorate --all'
-  git config --global core.excludesfile ~/.gitignore_global
-
-  ### zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  cp ${SCRIPT_PATH}/dotfiles/.zshrc ~/.zshrc
-  chsh -s $(which zsh)
+  #cp ${SCRIPT_PATH}/configs/Preferences ~/.config/BraveSoftware/Brave-Browser/Default/
 }
 
 # config greeter
@@ -83,4 +56,24 @@ ExecStart=
 ExecStart=-/usr/bin/agetty --autologin $USER_INPUT --noclear %I $TERM
 EOF
   sudo cp ${SCRIPT_PATH}/configs/override.conf /etc/systemmd/system/getty@tt1.service.d/
+}
+
+set_git_config() {
+	git config --global user.name "$USER_INPUT"
+  git config --global user.email "$EMAIL_INPUT"
+  git config --global color.ui auto
+  git config --global core.editor "nvim"
+  git config --global alias.st status
+  git config --global alias.co checkout
+  git config --global alias.br branch
+  git config --global alias.cm commit
+  git config --global alias.last 'log -1 HEAD'
+  git config --global alias.rv 'reset --soft HEAD~1'
+  git config --global alias.lg 'log --oneline --graph --decorate --all'
+  git config --global core.excludesfile ~/.gitignore_global
+}
+
+set_zsh_config() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  chsh -s $(which zsh)
 }
